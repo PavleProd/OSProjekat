e3252908a7658edcb560b2555aa20849cdf76f8c
Павле Продановић <pavleprod249@gmail.com>
2023-02-01 02:26:13 +0100

    push

--- build/src/BuddyAllocator.d
+++ build/src/BuddyAllocator.d
@@ -1,13 +1,10 @@
 build/src/BuddyAllocator.o: src/BuddyAllocator.cpp \
- src/../h/BuddyAllocator.h src/../h/hw.h src/../h/MemoryAllocator.h \
- src/../h/printing.hpp src/../h/syscall_c.h src/../h/PCB.h src/../h/SCB.h \
- src/../h/Scheduler.h
+ src/../h/BuddyAllocator.h src/../h/hw.h src/../h/printing.hpp \
+ src/../h/syscall_c.h src/../h/PCB.h src/../h/SCB.h src/../h/Scheduler.h
 
 src/../h/BuddyAllocator.h:
 
 src/../h/hw.h:
-
-src/../h/MemoryAllocator.h:
 
 src/../h/printing.hpp:
 
--- build/src/userMain.d
+++ build/src/userMain.d
@@ -1,1 +1,17 @@
-build/src/userMain.o: src/userMain.cpp
+build/src/userMain.o: src/userMain.cpp src/../h/BuddyAllocator.h \
+ src/../h/hw.h src/../h/printing.hpp src/../h/syscall_c.h src/../h/PCB.h \
+ src/../h/SCB.h src/../h/Scheduler.h
+
+src/../h/BuddyAllocator.h:
+
+src/../h/hw.h:
+
+src/../h/printing.hpp:
+
+src/../h/syscall_c.h:
+
+src/../h/PCB.h:
+
+src/../h/SCB.h:
+
+src/../h/Scheduler.h:
